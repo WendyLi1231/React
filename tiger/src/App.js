540eced36e2d0f9228d66f67885bd4b195b9d01f
WendyLi1231 <you@example.com>
2022-02-07 16:46:27 +0800

    init:settings

--- tiger/src/App.js
+++ tiger/src/App.js
@@ -1,21 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from '@/static/logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <img src={logo} alt="" />
+        <a>
+          千里之行，始于足下。
         </a>
       </header>
     </div>
