bb1ce07ad5b8be61213684799dfa6e127407eb0e
WendyLi1231 <you@example.com>
2022-02-07 17:22:26 +0800

    init：更改目录

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import './App.css';
-import logo from '@/static/logo.png';
+import "./App.css";
+import logo from "@/static/logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="" />
-        <a>
-          千里之行，始于足下。
-        </a>
+        <a>千里之行，始于足下。</a>
       </header>
     </div>
   );
